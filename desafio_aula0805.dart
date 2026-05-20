015b42df25cec0554457b8cd22d53f8b24a1deef
Fernanda Castilhos <fecastilhos@gmail.com>
2026-05-20 20:54:07 -0300

    atualização

--- desafio_aula0805.dart
+++ desafio_aula0805.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 void main() {
   List<Map<String, dynamic>> produtos = [
     {
@@ -83,13 +81,12 @@ void main() {
   ];
 
  
-  //map converte os itens da lista - (e) é cada  item da lista
+  //map converte os itens da lista - (e) é cada item da lista
   final listaFormatada = produtos.map((e) {
     String valorVendaVirgulaPraPonto = e['valor_venda'].toString().replaceAll(RegExp(r','), '.'); //variavel valorVendaVirgulaPraPonto = replace all está substutindo as vírgulas pelo pontos e tudo está convertido para string
     double valorVenda = double.parse(valorVendaVirgulaPraPonto);
     double valorCompra = double.parse(e['valor_compra']);//variavel valorCompra  = parse transforma a string em double
     double lucro = valorVenda - valorCompra;
-    
     
     return{
       "id": e['id'],
@@ -98,9 +95,9 @@ void main() {
       "valor_compra": valorCompra,
       "lucro": lucro,
     };//adicionando a chave lucro na lista
-  }).toList();//transforma para lista novamente
+  }).toList();//transforma para lista novamente(sempre que usar o map tem que usar o toList)
 
-print(listaFormatada);
+//print(listaFormatada);para testar se a lista está ok
 
  print("\n\nPRODUTOS\n");
 
@@ -109,6 +106,13 @@ print(listaFormatada);
   //*sendo elas o ID do produto, o nome e o lucro que esta dando
 
   //? Imprimir na tela conforme modelo abaixo:
+  listaFormatada.forEach((element){//executa uma ação para cada elemento, no caso para cada variavel
+    String id = element['id'].toString();
+    String produto = element ['produto'];
+    String lucro = (element ['lucro'] as double).transformarEmDinheiroBr();
+
+    print("ID: $id | PRODUTO: $produto | LUCRO POR VENDA: $lucro");
+  });//para imprimir como solicitado no exercício
 
   // print("ID: 1| PRODUTO: SABÃO | LUCRO POR VENDA: R$12,00");
   // print("ID: 1| PRODUTO: oleo | LUCRO POR VENDA: -R$7,00");
@@ -116,19 +120,76 @@ print(listaFormatada);
   print("\n\nMÉDIAS\n");
   //*Contexto: Agora você precisar gerar alguns relatórios sintéticos para o usuário:
 
+  //declaração das variáveis
+  double totalValorVenda = 0;
+  double totalValorCompra = 0;
+  double totalLucro = 0;
+  int quantidadeDaLucro = 0;
+  int quantidadeLucroAcimaDeDois = 0;
+
+  //percorre a lista e "pegar"o valor
+  listaFormatada.forEach((element){
+    totalLucro += element['lucro'];
+    totalValorVenda += element['valor_venda'];
+    totalValorCompra += element['valor_compra'];
+
+    //("\n\nMAIOR PARTE DOS PRODUTOS DA LUCRO?:\n"); para atender essa solicitação
+    if (element ['lucro'] > 0){ //testa se o lucro é maior que 0, se sim incrementa 
+      quantidadeDaLucro++;
+    }
+
+    //("\n\nMAIOR PARTE DOS PRODUTOS DA LUCRO ACIMA DE R\$2,00?:\n");para atender essa solicitação
+    if (element['lucro'] > 2){
+      quantidadeLucroAcimaDeDois++;//testa os valores de lucro acima de R$2 e incrementa
+    }
+
+  },);
+
   //? Imprimir a média do valor de venda: Média valor venda: R$21,00
+  print (
+  "Média valor venda: ${(totalValorVenda / listaFormatada.length).transformarEmDinheiroBr()}",
+  );//lenght (tamanho da lista)
+
   //? Imprimir a média do valor de compra: Média valor compra: R$18,00
-  //? Imprimir a média do lucro: Média valor compra: R$3,50
+  print (
+  "Média valor compra: ${(totalValorCompra / listaFormatada.length).transformarEmDinheiroBr()}",
+  );
+
+  //? Imprimir a média do lucro: Média valor lucro: R$3,50
+  print (
+  "Média valor lucro: ${(totalLucro / listaFormatada.length).transformarEmDinheiroBr()}",
+  );
 
   print("\n\nMAIOR PARTE DOS PRODUTOS DA LUCRO?:\n");
   //? imprimir "SIM" ou "NÃO"
+  print(quantidadeDaLucro > (listaFormatada.length / 2) ? "SIM" : "NÃO");
 
   print("\n\nMAIOR PARTE DOS PRODUTOS DA LUCRO ACIMA DE R\$2,00?:\n");
   //? imprimir "SIM" ou "NÃO"
+  print(quantidadeLucroAcimaDeDois > (quantidadeDaLucro / 2) ? "SIM" : "NÃO");
 
   print("\n\nPRODUTOS QUE DÃO PREJUIZO:\n");
   //*Contexto: Agora você precisar gerar um relatório analítico mostrando
   //*os produtos que dão prejuizo:
 
+  //o where vai retornar uma com uma "sublista" com os itens que atenderem o teste lógico e ir filtrando
+  final listaDePrejuizo = listaFormatada.where((element) => element ['lucro'] < 0);
+
+  //forEach para percorrer a lista
+  listaDePrejuizo.forEach((element) {
+    String id = element['id'].toString();
+    String produto = element['produto'];
+    String lucro = (element ['lucro'] as double).transformarEmDinheiroBr();
+    print("ID: $id | PRODUTO: $produto | PREJUIZO:$lucro");
+  },);
+
   //? imprimir produtos que dão prejuizo, para cada linha imprimir conforme exemplo: ID: 1 | PRODUTO: SABÃO | PREJUIZO: -R$3,00
 }
+
+extension FormatadoresDeDinheiro on double {
+  String transformarEmDinheiroBr() => this.isNegative 
+  ? "-R\$${abs().toStringAsFixed(2).replaceAll('.', ',')}" //true se 
+  : "R\$${abs().toStringAsFixed(2).replaceAll('.', ',')}"; //false senao
+// a extensão é para testar se o número é negativo, se sim recebe o - (como o $ é um caracter especial foi usado \$, porque ele é usado para concatenar string)
+//e o restante é para formatar o valor (abs(double))
+} 
---
+++ exercicio2_aula1305.dart
@@ -0,0 +1,109 @@
+class Pedido{
+  final String id;
+  final double dataCriacao;
+  final String status;
+
+  //construtor nomeado para converter(receber) um Map em uma lista como solicitado no exercício e transformar 
+  Pedido.fromMap(Map map)
+  : idPedido = map['id_pedido'], //id no Map na "posição/local" id no Map
+    dataCriacao = map['data_criacao'],
+    status = map['status'];
+} 
+
+class Cliente {
+  final String id;
+  final String nome;
+  final String email; 
+
+  //construtor nomeado para converter(receber) um Map em uma lista como solicitado no exercício e transformar 
+  Cliente.fromMap(Map map)
+  : id = map['id_cliente'],
+    nome = map['nome'], 
+    email = map['email'];
+}
+
+class Pagamento{
+  final String metodo;
+  final int parcelas;
+  final bool confirmado;
+
+//construtor nomeado para converter(receber) um Map em uma lista como solicitado no exercício e transformar 
+  Pagamento.fromMap(Map map)
+  : metodo = map['metodo'],
+    parcelas = map['parcelas'], 
+    confirmado = map['confirmado'];
+}
+
+class Produto{
+ final String id;
+ final String nome;
+ final double preco;
+ final int quantidade;
+
+//construtor nomeado para converter(receber) um Map em uma lista como solicitado no exercício e transformar 
+  Produto.fromMap(Map map)
+  : id = map['id_cliente'],
+    nome = map['nome'], 
+    preco = map['preco'];
+    quantidade = map['quantidade'];
+}
+void main(){
+  //variável que vai guardar a lista
+  final listaConvertida = listaPedidos.map((e) => Pedido.fromMap(e)).toList();
+
+  //* 4 percorro minha lista de objeto via forEach para imprimir o nome de cada aluno
+  listaConvertida.forEach((element) => print(element.idPedido));
+}
+
+
+final listaPedidos = [
+  {
+    "id_pedido": "REQ-9872",
+    "data_criacao": "2026-05-14T10:00:00Z",
+    "status": "processando",
+    "cliente": {
+      "id_cliente": "USR-441",
+      "nome": "Ana Silva",
+      "email": "ana.silva@email.com",
+    },
+    "pagamento": {
+      "metodo": "cartao_credito",
+      "parcelas": 3,
+      "confirmado": true,
+    },
+    "itens": [
+      {
+        "id_produto": "PROD-001",
+        "nome": "Teclado Mecânico RGB",
+        "preco_unitario": 349.90,
+        "quantidade": 1,
+      },
+
+      {
+        "id_produto": "PROD-012",
+        "nome": "Mouse Pad Extendido",
+        "preco_unitario": 89.90,
+        "quantidade": 2,
+      },
+    ],
+  },
+  {
+    "id_pedido": "REQ-9873",
+    "data_criacao": "2026-05-14T10:15:00Z",
+    "status": "entregue",
+    "cliente": {
+      "id_cliente": "USR-102",
+      "nome": "Carlos Souza",
+      "email": "carlos.s@email.com",
+    },
+    "pagamento": {"metodo": "pix", "parcelas": 1, "confirmado": true},
+    "itens": [
+      {
+        "id_produto": "PROD-055",
+        "nome": "Fone de Ouvido Bluetooth",
+        "preco_unitario": 199.00,
+        "quantidade": 1,
+      },
+    ],
+  },
+];
